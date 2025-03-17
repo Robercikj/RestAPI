dc106a266c8f7e25a71f394b0e5fde47b1ef9541
Robert <robertjakubowski61@gmail.com>
2025-03-17 21:49:10 +0100

    fixed

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,9 +1,6 @@
 package com.crud.tasks.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +8,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "tasks")
+@Entity
+@Table(name= "tasks")
 public class Task {
     @Id
     @GeneratedValue
