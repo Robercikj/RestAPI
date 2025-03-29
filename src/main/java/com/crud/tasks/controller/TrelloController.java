1a122d73f8e4f503057eba47263de6becb883317
Robert <robertjakubowski61@gmail.com>
2025-03-29 16:50:09 +0100

    21.2

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });
+    }
+    @GetMapping("/filtered")
+    public List<TrelloBoardDto> getFilteredTrelloBoards() {
+        return trelloClient.getTrelloBoards().stream()
+                .filter(board -> board.getId() != null && board.getName() != null)
+                .filter(board -> board.getName().contains("Kodilla"))
+                .collect(Collectors.toList());
+    }
+
+}
