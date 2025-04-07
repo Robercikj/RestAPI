0e2266c7b20b6ecf0c476af3eda59f26869d83f7
Robert <robertjakubowski61@gmail.com>
2025-04-07 10:03:32 +0200

    dodanie set

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -31,6 +31,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(adjustTaskMessage(mail.getMessage()));
+        mail.getToCc().ifPresent(mailMessage::setCc);
         return mailMessage;
     }
 
